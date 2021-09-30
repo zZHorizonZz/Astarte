124c5fabb64b3c185c208aece3849fd5cb7c0b83
Horizon <danfiala23@gmail.com>
2021-09-30 23:54:14 +0200

    Work on method block added unit test

--- src/main/java/com/github/interpreter/language/method/MethodBlock.java
+++ src/main/java/com/github/interpreter/language/method/MethodBlock.java
@@ -1,20 +1,45 @@
 package com.github.interpreter.language.method;
 
 import com.github.interpreter.language.logic.Block;
+import com.github.interpreter.language.logic.FieldBlock;
+import com.github.interpreter.parser.expression.Expression;
+import com.github.interpreter.parser.expression.FieldExpression;
 import com.github.interpreter.parser.method.MethodBlockDeclarator;
 
 import java.util.LinkedList;
 import java.util.List;
 
+//TODO Add something like block identifier.
 public class MethodBlock {
 
-    private List<Block> block = new LinkedList<>();
+    private final List<Block> blockList = new LinkedList<>();
 
     public void construct(MethodBlockDeclarator declarator) {
+        if (declarator.getBody() == null) {
+            return;
+        }
 
+        List<Expression> body = declarator.getBody();
+        for (Expression expression : body) {
+            if (expression instanceof FieldExpression) {
+                blockList.add(new FieldBlock((FieldExpression) expression));
+            }
+        }
     }
 
-    public List<Block> getBlock() {
-        return block;
+    public void invoke() {
+        if (blockList.isEmpty()) {
+            return;
+        }
+
+        for (Block block : blockList) {
+            if (block instanceof FieldBlock) {
+                ((FieldBlock) block).initialize();
+            }
+        }
+    }
+
+    public List<Block> getBlockList() {
+        return blockList;
     }
 }
